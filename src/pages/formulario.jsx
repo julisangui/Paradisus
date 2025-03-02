84e05023745785f2b06445fabfff95bba55428b1
Julian <juliangel2003@gmail.com>
2025-03-02 03:51:21 -0300

    actualizaciones

--- src/pages/formulario.jsx
+++ src/pages/formulario.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Header from "../components/header.jsx";
 
 const Formulario = () => {
-    const [formData, setFormData] = useState({
+    const [dato, ingresarDato] = useState({
         nombre: "",
         cantidadPersonas: "",
         email: "",
@@ -14,87 +14,93 @@ const Formulario = () => {
         fechaExpiracion: "",
         codigoVerificacion: "",
         titularTarjeta: ""
-
     });
 
+    const [formEnviado, setFormEnviado] = useState(false);
+
     const handleChange = (e) => {
-        setFormData({ ...formData, [e.target.name]: e.target.value });
+        ingresarDato({ ...dato, [e.target.name]: e.target.value });
     };
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log("Reserva enviada", formData);
-        alert("¡Reserva enviada con éxito!");
+        setFormEnviado(true);
     };
 
     return (
         <>
             <Header />
             <div className="formulario">
-                <form onSubmit={handleSubmit}>
-                    <div className="fila">
-                        <div>
-                            <label>Nombre Completo</label>
-                            <input type="text" name="nombre" value={formData.nombre} onChange={handleChange} required />
+                {formEnviado ? (
+                    <div id="mensaje">
+                        <p>¡Reserva enviada correctamente!</p>
+                        <i className="bi bi-check-circle-fill"></i>
+                    </div> ) : (
+                    <form onSubmit={handleSubmit}>
+                        <div className="fila">
+                            <div>
+                                <label>Nombre Completo</label>
+                                <input type="text" name="nombre" value={dato.nombre} onChange={handleChange} required />
+                            </div>
+                            <div>
+                                <label>Cantidad de personas</label>
+                                <input type="number" name="cantidadPersonas" value={dato.cantidadPersonas} onChange={handleChange} required />
+                            </div>
                         </div>
-                        <div>
-                            <label>Cantidad de personas</label>
-                            <input type="number" name="cantidadPersonas" value={formData.cantidadPersonas} onChange={handleChange} required />
+                        <div className="fila">
+                            <div>
+                                <label>Correo Electrónico</label>
+                                <input type="email" name="email" value={dato.email} onChange={handleChange} required />
+                            </div>
+                            <div>
+                                <label>Teléfono</label>
+                                <input type="tel" name="telefono" value={dato.telefono} onChange={handleChange} required />
+                            </div>
                         </div>
-                    </div>
-                    <div className="fila">
-                        <div>
-                            <label>Correo Electrónico</label>
-                            <input type="email" name="email" value={formData.email} onChange={handleChange} required />
+                        <div className="formulario-select">
+                            <label>Tipo de Alojamiento</label>
+                            <select name="tipo" value={dato.tipo} onChange={handleChange} required>
+                                <option hidden value=""></option>
+                                <option value="habitacion">Habitación</option>
+                                <option value="villa">Villa Tortuga</option>
+                                <option value="villa">Villa Delfín</option>
+                                <option value="villa">Villa Flamengo</option>
+                                <option value="villa">Villa Perla</option>
+                            </select>
                         </div>
-                        <div>
-                            <label>Teléfono</label>
-                            <input type="tel" name="telefono" value={formData.telefono} onChange={handleChange} required />
+                        <div className="fila">
+                            <div>
+                                <label>Fecha de ingreso</label>
+                                <input type="date" name="ingreso" value={dato.ingreso} onChange={handleChange} required />
+                            </div>
+                            <div>
+                                <label>Fecha de salida</label>
+                                <input type="date" name="salida" value={dato.salida} onChange={handleChange} required />
+                            </div>
                         </div>
-                    </div>
-                    <div className="formulario-select">
-                        <label>Tipo de Alojamiento</label>
-                        <select name="tipo" value={formData.tipo} onChange={handleChange} required>
-                            <option hidden value=""></option>
-                            <option value="habitacion">Habitación</option>
-                            <option value="villa">Villa Tortuga</option>
-                            <option value="villa">Villa Delfin</option>
-                            <option value="villa">Villa Flamengo</option>
-                            <option value="villa">Villa Perla</option>
-                        </select>
-                    </div>
-                    <div className="fila">
-                        <div>
-                            <label>Fecha de ingreso</label>
-                            <input type="date" name="ingreso" value={formData.ingreso} onChange={handleChange} required />
+                        <div className="fila">
+                            <div>
+                                <label>Titular de la tarjeta</label>
+                                <input type="text" name="titularTarjeta" value={dato.titularTarjeta} onChange={handleChange} required />
+                            </div>
+                            <div>
+                                <label>Número de la tarjeta</label>
+                                <input type="number" name="numeroTarjeta" value={dato.numeroTarjeta} onChange={handleChange} placeholder="Ingrese un número cualquiera" required />
+                            </div>
                         </div>
-                        <div>
-                            <label>Fecha de salida</label>
-                            <input type="date" name="salida" value={formData.salida} onChange={handleChange} required />
+                        <div className="fila">
+                            <div>
+                                <label>Fecha de expiración</label>
+                                <input type="text" name="fechaExpiracion" value={dato.fechaExpiracion} onChange={handleChange} placeholder="mm/aa" required />
+                            </div>
+                            <div>
+                                <label>CVC/CVV</label>
+                                <input type="number" name="codigoVerificacion" value={dato.codigoVerificacion} onChange={handleChange} placeholder="Ingrese un número cualquiera" required />
+                            </div>
                         </div>
-                    </div>
-                    <div className="fila">
-                        <div>
-                            <label>Titular de la tarjeta</label>
-                            <input type="text" name="titularTarjeta" value={formData.titularTarjeta} onChange={handleChange} required />
-                        </div>
-                        <div>
-                            <label>Numero de la tarjeta</label>
-                            <input type="number" name="numeroTarjeta" value={formData.numeroTarjeta} onChange={handleChange} placeholder="Ingrese un numero cualquiera" required />
-                        </div>
-                    </div>
-                    <div className="fila">
-                        <div>
-                            <label>Fecha de expiración</label>
-                            <input type="text" name="fechaExpiracion" value={formData.fechaExpiracion} onChange={handleChange} placeholder="mm/aa" required />
-                        </div>
-                        <div>
-                            <label>CVC/CVV</label>
-                            <input type="number" name="codigoVerificacion" value={formData.codigoVerificacion} onChange={handleChange} placeholder="Ingrese un numero cualquiera" required />
-                        </div>
-                    </div>
-                    <button type="submit">Reservar</button>
-                </form>
+                        <button type="submit">Reservar</button>
+                    </form>
+                )}
             </div>
         </>
     );
