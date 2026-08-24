24104a3e8176990bef9c14e91d2324ee7bc26ec9
Julian <juliangel2003@gmail.com>
2026-08-24 00:25:34 -0300

    actualizacion

--- src/main.jsx
+++ src/main.jsx
@@ -25,7 +25,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <BrowserRouter basename="/Paradisus">
       <Routes>
         <Route
-          path="*"
+          path="/"
           element={
             <>
               <Header />
